a3673e977cedd9521d05e93c76471ff826da1857
Michail <mihasik323@gmail.com>
2020-07-07 19:22:47 +0300

    added weekly-products section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function() {
-
+    $('.weekly-products__slider').slick({
+        prevArrow: '<button class="slick-prev slick-arrow"><svg class="lnr lnr-chevron-left"><use xlink:href="#lnr-chevron-left"></use></svg></button>',
+        nextArrow: '<button class="slick-next slick-arrow"><svg class="lnr lnr-chevron-right"><use xlink:href="#lnr-chevron-right"></use></svg></button>',
+        infinite: true,
+        autoplay: 3000,
+    });
 });
