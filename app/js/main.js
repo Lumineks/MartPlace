398f20415e1d089bfebc367e666a21741e78c1f1
Michail <mihasik323@gmail.com>
2020-07-10 18:45:19 +0300

    added news section

--- app/js/main.js
+++ app/js/main.js
@@ -16,6 +16,15 @@ $(function() {
         slidesToScroll: 1,
         variableWidth: true,
     });
+    $('.feedback__inner').slick({
+        prevArrow: '<button class="slick-arrow slick-arrow--prev"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20" height="20" viewBox="0 0 20 20"><path fill="#7347c1" d="M14 20c0.128 0 0.256-0.049 0.354-0.146 0.195-0.195 0.195-0.512 0-0.707l-8.646-8.646 8.646-8.646c0.195-0.195 0.195-0.512 0-0.707s-0.512-0.195-0.707 0l-9 9c-0.195 0.195-0.195 0.512 0 0.707l9 9c0.098 0.098 0.226 0.146 0.354 0.146z"></path></svg></button>',
+        nextArrow: '<button class="slick-arrow slick-arrow--next"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20" height="20" viewBox="0 0 20 20"><path fill="#7347c1" d="M5 20c-0.128 0-0.256-0.049-0.354-0.146-0.195-0.195-0.195-0.512 0-0.707l8.646-8.646-8.646-8.646c-0.195-0.195-0.195-0.512 0-0.707s0.512-0.195 0.707 0l9 9c0.195 0.195 0.195 0.512 0 0.707l-9 9c-0.098 0.098-0.226 0.146-0.354 0.146z"></path></svg></button>',
+        infinite: true,
+        autoplay: 3000,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        variableWidth: true,
+    });
 
     var mixer = mixitup('.newest-products__items');
 
