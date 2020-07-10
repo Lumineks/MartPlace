fed6ff4a600790f0e29046bc97ca84a066a2b0a5
Michail <mihasik323@gmail.com>
2020-07-10 12:15:00 +0300

    added followers, statistics and reasons sections

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,17 @@ $(function() {
         nextArrow: '<button class="slick-arrow slick-arrow--next"><svg class="lnr lnr-chevron-right"><use xlink:href="#lnr-chevron-right"></use></svg></button>',
         infinite: true,
         autoplay: 3000,
-        appendArrows: '.slider-head__arrows',
+        appendArrows: '.weekly-products__arrows',
+    });
+    $('.followers__inner').slick({
+        prevArrow: '<button class="slick-arrow slick-arrow--prev"><svg class="lnr lnr-chevron-left"><use xlink:href="#lnr-chevron-left"></use></svg></button>',
+        nextArrow: '<button class="slick-arrow slick-arrow--next"><svg class="lnr lnr-chevron-right"><use xlink:href="#lnr-chevron-right"></use></svg></button>',
+        infinite: true,
+        autoplay: 3000,
+        appendArrows: '.followers__arrows',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        variableWidth: true,
     });
 
     var mixer = mixitup('.newest-products__items');
