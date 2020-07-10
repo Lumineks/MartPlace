a77417b05db415d558547446c76b82034ee60557
Michail <mihasik323@gmail.com>
2020-07-10 10:12:45 +0300

    fixed homework

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,22 @@
 $(function() {
     $('.weekly-products__slider').slick({
-        prevArrow: '<button class="slick-prev slick-arrow"><svg class="lnr lnr-chevron-left"><use xlink:href="#lnr-chevron-left"></use></svg></button>',
-        nextArrow: '<button class="slick-next slick-arrow"><svg class="lnr lnr-chevron-right"><use xlink:href="#lnr-chevron-right"></use></svg></button>',
+        prevArrow: '<button class="slick-arrow slick-arrow--prev"><svg class="lnr lnr-chevron-left"><use xlink:href="#lnr-chevron-left"></use></svg></button>',
+        nextArrow: '<button class="slick-arrow slick-arrow--next"><svg class="lnr lnr-chevron-right"><use xlink:href="#lnr-chevron-right"></use></svg></button>',
         infinite: true,
         autoplay: 3000,
+        appendArrows: '.slider-head__arrows',
     });
 
     var mixer = mixitup('.newest-products__items');
 
     $('.greeting__list').on('click', function() {
         $('.greeting__list').toggleClass('active')
+    });
+
+    $(".stars").rateYo({
+        rating: 4.5,
+        starWidth: "16px",
+        readOnly: true,
+        ratedFill: "#ffc000"
     });
 });
