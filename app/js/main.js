a6d34fdee63211cadec711218e96d8dae37ecb86
Michail <mihasik323@gmail.com>
2020-07-19 10:51:01 +0300

    Lesson 12 fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,15 @@
-$(function() {
+$(function () {
     $('.weekly-products__slider').slick({
-        prevArrow: '<button class="slick-arrow slick-arrow--prev"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="14" height="14" viewBox="0 0 20 20"><path fill="#ffffff" d="M14 20c0.128 0 0.256-0.049 0.354-0.146 0.195-0.195 0.195-0.512 0-0.707l-8.646-8.646 8.646-8.646c0.195-0.195 0.195-0.512 0-0.707s-0.512-0.195-0.707 0l-9 9c-0.195 0.195-0.195 0.512 0 0.707l9 9c0.098 0.098 0.226 0.146 0.354 0.146z"></path></svg></button>',
-        nextArrow: '<button class="slick-arrow slick-arrow--next"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="14" height="14" viewBox="0 0 20 20"><path fill="#ffffff" d="M5 20c-0.128 0-0.256-0.049-0.354-0.146-0.195-0.195-0.195-0.512 0-0.707l8.646-8.646-8.646-8.646c-0.195-0.195-0.195-0.512 0-0.707s0.512-0.195 0.707 0l9 9c0.195 0.195 0.195 0.512 0 0.707l-9 9c-0.098 0.098-0.226 0.146-0.354 0.146z"></path></svg></button>',
+        prevArrow: '<button class="slick-arrow slick-arrow--prev lnr-chevron-left"></button>',
+        nextArrow: '<button class="slick-arrow slick-arrow--next lnr-chevron-right"></button>',
         infinite: true,
         autoplay: 3000,
         appendArrows: '.weekly-products__arrows',
+
     });
     $('.followers__inner').slick({
-        prevArrow: '<button class="slick-arrow slick-arrow--prev"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20" height="20" viewBox="0 0 20 20"><path fill="#ffffff" d="M14 20c0.128 0 0.256-0.049 0.354-0.146 0.195-0.195 0.195-0.512 0-0.707l-8.646-8.646 8.646-8.646c0.195-0.195 0.195-0.512 0-0.707s-0.512-0.195-0.707 0l-9 9c-0.195 0.195-0.195 0.512 0 0.707l9 9c0.098 0.098 0.226 0.146 0.354 0.146z"></path></svg></button>',
-        nextArrow: '<button class="slick-arrow slick-arrow--next"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20" height="20" viewBox="0 0 20 20"><path fill="#ffffff" d="M5 20c-0.128 0-0.256-0.049-0.354-0.146-0.195-0.195-0.195-0.512 0-0.707l8.646-8.646-8.646-8.646c-0.195-0.195-0.195-0.512 0-0.707s0.512-0.195 0.707 0l9 9c0.195 0.195 0.195 0.512 0 0.707l-9 9c-0.098 0.098-0.226 0.146-0.354 0.146z"></path></svg></button>',
+        prevArrow: '<button class="slick-arrow slick-arrow--prev lnr-chevron-left"><</button>',
+        nextArrow: '<button class="slick-arrow slick-arrow--next lnr-chevron-right"></button>',
         infinite: true,
         autoplay: 3000,
         appendArrows: '.followers__arrows',
@@ -17,8 +18,8 @@ $(function() {
         variableWidth: true,
     });
     $('.feedback__inner').slick({
-        prevArrow: '<button class="slick-arrow slick-arrow--prev"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20" height="20" viewBox="0 0 20 20"><path fill="#000000" d="M14 20c0.128 0 0.256-0.049 0.354-0.146 0.195-0.195 0.195-0.512 0-0.707l-8.646-8.646 8.646-8.646c0.195-0.195 0.195-0.512 0-0.707s-0.512-0.195-0.707 0l-9 9c-0.195 0.195-0.195 0.512 0 0.707l9 9c0.098 0.098 0.226 0.146 0.354 0.146z"></path></svg></button>',
-        nextArrow: '<button class="slick-arrow slick-arrow--next"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20" height="20" viewBox="0 0 20 20"><path fill="#7347c1" d="M5 20c-0.128 0-0.256-0.049-0.354-0.146-0.195-0.195-0.195-0.512 0-0.707l8.646-8.646-8.646-8.646c-0.195-0.195-0.195-0.512 0-0.707s0.512-0.195 0.707 0l9 9c0.195 0.195 0.195 0.512 0 0.707l-9 9c-0.098 0.098-0.226 0.146-0.354 0.146z"></path></svg></button>',
+        prevArrow: '<button class="slick-arrow slick-arrow--prev lnr-chevron-left"></button>',
+        nextArrow: '<button class="slick-arrow slick-arrow--next lnr-chevron-right"></button>',
         infinite: true,
         autoplay: 3000,
         slidesToShow: 2,
@@ -27,16 +28,32 @@ $(function() {
     });
 
 
-
-    $('.greeting__list').on('click', function() {
+    $('.greeting__list').on('click', function () {
         $('.greeting__list').toggleClass('active')
     });
 
     $(".stars").rateYo({
         rating: 4.5,
-        starWidth: "17px",
+        starWidth: "14px",
         readOnly: true,
         ratedFill: "#ffc000"
     });
+
+    if ($(window).width() < 768) {
+        $('.header__user').addClass('phone');
+    }
+
+    $('.header__user').click(function () {
+        return false;
+    });
+    $('.header__icon--cart').click(function () {
+        return false;
+    });
+
+    $('.header__burger').on('click', function () {
+        $('.header__burger').toggleClass('active');
+        $('.header__list').toggleClass('active');
+    });
+
     var mixer = mixitup('.newest-products__items');
 });
